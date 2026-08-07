d7af3c5981e9a3ee3a7d43fd9c2a20c853829697
Mohd Zaheeruddin <183212672+mdzaheerjk@users.noreply.github.com>
2026-08-08 03:56:01 +0530

    config added

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ os.environ['TF_ENABLE_ONEDNN_OPTS']='0'
 SAMPLE_RATE=22050
 N_MFCC=52
 
-HF_REPO_ID=''
-HF_MODEL_FILENAME=''
+HF_REPO_ID='zaheerjk/AI-Powered-Heart-Murmur-Detection-System'
+HF_MODEL_FILENAME='Models/lstm_model.h5'
